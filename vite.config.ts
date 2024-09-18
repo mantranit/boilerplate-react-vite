48e9bd2264a5a598a350b1e37f8fc5e167c14d00
Man Tran <tranduyminhman@gmail.com>
2024-09-18 11:22:02 +0700

    update setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+import path from 'path';
+
+const PORT = 3003;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      overlay: {
+        position: 'tl',
+        initialIsOpen: false,
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /^src(.+)/,
+        replacement: path.join(__dirname, 'src/$1'),
+      },
+    ],
+  },
+  server: { port: PORT, host: true },
+  preview: { port: PORT, host: true },
 });
