c83dba47b2d76532adc6590b6e658db05202320b
Man Tran <tranduyminhman@gmail.com>
2024-09-18 22:23:14 +0700

    page and route to ts

---
+++ src/config-global.ts
@@ -0,0 +1,21 @@
+import { paths } from 'src/routes/paths';
+
+import packageJson from '../package.json';
+
+// ----------------------------------------------------------------------
+
+export const CONFIG = {
+  appName: packageJson.name,
+  appVersion: packageJson.version,
+  serverUrl: import.meta.env.VITE_SERVER_URL ?? '',
+  assetsDir: import.meta.env.VITE_ASSETS_DIR ?? '',
+  /**
+   * Auth
+   * @method jwt | amplify | firebase | supabase | auth0
+   */
+  auth: {
+    method: 'jwt',
+    skip: false,
+    redirectPath: paths.dashboard.root,
+  },
+};
