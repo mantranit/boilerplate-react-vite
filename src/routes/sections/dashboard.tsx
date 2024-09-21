ce230e97e7835afded1284faeba02bf149e4c215
Man Tran <tranduyminhman@gmail.com>
2024-09-22 02:28:13 +0700

    remove

--- src/routes/sections/dashboard.tsx
+++ src/routes/sections/dashboard.tsx
@@ -10,15 +10,6 @@ import { AuthGuard } from 'src/auth/guard';
 
 // ----------------------------------------------------------------------
 
-const IndexPage = lazy(() => import('src/pages/dashboard/one'));
-const PageTwo = lazy(() => import('src/pages/dashboard/two'));
-const PageThree = lazy(() => import('src/pages/dashboard/three'));
-const PageFour = lazy(() => import('src/pages/dashboard/four'));
-const PageFive = lazy(() => import('src/pages/dashboard/five'));
-const PageSix = lazy(() => import('src/pages/dashboard/six'));
-
-// ----------------------------------------------------------------------
-
 const UserListPage = lazy(() => import('src/pages/user/list'));
 const RoleListPage = lazy(() => import('src/pages/role/list'));
 const EmployeeListPage = lazy(() => import('src/pages/employee/list'));
@@ -44,17 +35,6 @@ export const dashboardRoutes = [
     // path: 'dashboard',
     element: CONFIG.auth.skip ? <>{layoutContent}</> : <AuthGuard>{layoutContent}</AuthGuard>,
     children: [
-      { element: <IndexPage />, index: true },
-      { path: 'two', element: <PageTwo /> },
-      { path: 'three', element: <PageThree /> },
-      {
-        path: 'group',
-        children: [
-          { element: <PageFour />, index: true },
-          { path: 'five', element: <PageFive /> },
-          { path: 'six', element: <PageSix /> },
-        ],
-      },
       { path: 'leave-types', element: <LeaveTypeListPage /> },
       { path: 'submit-leave', element: <LeaveTypeListPage /> },
       { path: 'employee-details', element: <EmployeeDetailsViewPage /> },
